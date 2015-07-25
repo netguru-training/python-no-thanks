da51f128e130d2053146d02b14aa38a1ce56fbc1
Divoolej <divoolej@gmail.com>
2015-07-25 17:33:34 +0200

    Add validations and associations to Image model. Add Image model specs.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,14 @@
 class Image < ActiveRecord::Base
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+  validates :user, :title, :description, :gallery, presence: true
+
   has_attached_file :picture,
     styles: { medium: "300x300>", thumb: "100x100>" },
     default_url: ":style/missing.png",
     storage: :dropbox,
     dropbox_credentials: Rails.root.join("config/dropbox.yml"),
     dropbox_options: {}
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+
+  belongs_to :user
+  belongs_to :gallery
 end
---
+++ spec/models/image_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Image, type: :model do
+  it { should validate_presence_of :user  }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :gallery }
+end
