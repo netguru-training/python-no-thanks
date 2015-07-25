99e2120ea979483a0eb60d19811b8ea742a4fecb
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2015-07-25 19:21:20 +0200

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+user1 = User.create!(email: "jan.kowalski@gmail.com", password: "12345678", password_confirmation: "12345678", name: "Jan Kowalski" )
+
+
+gallery1 = Gallery.new(user: user1, title: "Wakacje 2015", description: "2 tyg. wyjazd na mazury")
+
+image1 = Image.new(user: user1, gallery: gallery1, picture: File.new("#{Rails.root}/app/assets/images/sample3.jpg"), title: "sunflower", description:"a beautyfull flower")
+
+gallery1.save
+image1.save
