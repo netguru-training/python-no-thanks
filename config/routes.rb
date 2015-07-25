56b002cc34fac16d363218df501fce92fcc8fba8
Velsu <riftmage50@gmail.com>
2015-07-25 18:55:27 +0200

    Refactor profie routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
 
-  scope '/profiles' do
-    match '/:id', to: 'profile_pages#show', as: "show_profile", via: [:get]
-  end
-
+  get '/profiles/:id', to: 'profile_pages#show', as: 'show_profile'
   resources :users
   resources :galleries do
     member do
