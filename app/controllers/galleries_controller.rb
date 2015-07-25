150b2fc08b879bf61991105b6d30e5698a2a330d
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2015-07-25 17:50:05 +0200

    add gallery controller

---
+++ app/controllers/galleries_controller.rb
@@ -0,0 +1,32 @@
+class GalleriesController < ApplicationController
+  
+  expose(:gallery)
+
+  def index
+    @user = User.find(params[:user_id])
+    @galleries = @user.galleries
+  end
+
+  def show
+  end
+
+  def update
+    if @gallery.update_attributes(secure_params)
+      redirect_to user_gallery_path(@gallery.user, @gallery), :notice => "Gallery updated."
+    else
+      redirect_to user_gallery_path(@gallery.user, @gallery), :alert => "Unable to update gallery."
+    end
+  end
+
+  def destroy
+    @gallery.destroy
+    redirect_to user_gallery_path(@gallery.user, @gallery), :notice => "Gallery deleted."
+  end
+
+  private
+
+  def secure_params
+    params.require(:gallery).permit()
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
-  resources :users
 
   scope '/profiles' do
     match '/:id', to: 'profile_pages#show', as: "show_profile", via: [:get]
   end
+
+  resources :users do
+    resources :galleries
+  end
+
 end
