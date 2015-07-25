f8a4e193494ee150f527086a2fc1bb55c9496663
Wojzag <wojaqu@gmail.com>
2015-07-25 15:14:39 +0200

    Add Image model and Dropbox configuration sample

---
+++ app/models/image.rb
@@ -0,0 +1,7 @@
+class Image < ActiveRecord::Base  
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":style/missing.png",
+    storage: :dropbox,
+    dropbox_credentials: Rails.root.join("config/dropbox.yml"),
+    dropbox_options: {}
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+end
