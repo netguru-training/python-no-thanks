7f72eb06a6c00a185b4ec691ca19fb4d64c246e2
Wojzag <wojaqu@gmail.com>
2015-07-26 12:00:49 +0200

    Add tags and searchbox functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,15 @@
 class HomeController < ApplicationController
-  expose(:images) { Image.all }
+  expose(:images) { find_images }
+  
+  private
+  
+  def find_images
+    if tag = params[:tag]
+      Image.tagged_with([tag], :any => true)
+    elsif word = params[:search]
+      Image.where('title ILIKE ?', "%#{word}%")
+    else
+      Image.all
+    end
+  end
 end
