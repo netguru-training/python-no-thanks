c3f57c2fa86b2ffdac0eca5f8627ad441d7329ad
Divoolej <divoolej@gmail.com>
2015-07-26 08:01:00 +0200

    Add basic Galleries controller spec for testing the #create action

---
+++ spec/controllers/galleries_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe GalleriesController, type: :controller do
+  describe "POST #create" do
+    context "with valid params" do
+      let(:params) do
+        {
+          gallery: { title: "MyTitle", description: "MyDesc", images_attributes: [{ picture_file_name: 'room-controller-valid.jpg' }] }
+        }
+      end
+      subject { post :create, params }
+
+      it "sth" do
+        subject
+        expect(response).to be_successful
+      end
+    end
+  end
+end
